aa7737c454753a6d4b63858f1e89ac5cf5d54803
Brendon Rabelo <rabelobrendon@gmail.com>
2026-03-05 22:01:47 -0300

    reiniciando tudo

--- backend/src/controllers/user.controller.ts
+++
@@ -1,3 +0,0 @@
-import { Request, Response } from "express";
-import { User } from "../types/user.types";
-import {bd}
--- backend/src/server.ts
+++
@@ -1,7 +0,0 @@
-import { app } from "./app"
-
-const PORT = "3000"
-
-app.listen(PORT, () => {
-    console.log("O site está funcionando em http://localhost:3000")
-})
