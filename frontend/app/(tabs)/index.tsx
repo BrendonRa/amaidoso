bb8132f84d21edf1a0ce2c5202af0fa90bdc35fc
Gabriel <gabrielantonny04@gmail.com>
2026-03-23 21:21:24 -0300

    Primeira tela inicio feita

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -9,51 +9,9 @@ import { Link } from 'expo-router';
 import { useEffect, useState } from 'react';
 import api from '../services/api';
 
+
+import { Redirect } from 'expo-router';
+
 export default function HomeScreen() {
-  const [products, setProducts] = useState<any>([]);
-
-  useEffect(() => {
-    api.get("/weatherforecast")
-      .then((response) => {
-        console.log(response.data);
-        setProducts(response.data);
-      })
-      .catch((err) => {
-        console.error("ops! ocorreu um erro" + err);
-      });
-  }, []);
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText>No dia {products[0]?.date} a temperatura será de {products[0]?.temperatureC}°</ThemedText>
-        <HelloWave />
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+  return <Redirect href="/tela_inicio1" />;
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
---
+++ frontend/app/(tabs)/tela_inicio1.tsx
@@ -0,0 +1,75 @@
+import { View, Image, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
+export default function TelaInicio() {
+  return (
+    <View style={styles.container}>
+
+      <Image
+        source={require('../../assets/images/logo.jpeg')}
+        style={styles.logo}
+      />
+
+      <Text style={styles.title}>Bem-vindo ao Amaidoso</Text>
+
+      <Text style={styles.subtitle}>
+        Crie uma conta e comece a usar
+      </Text>
+
+      <TouchableOpacity onPress={() => console.log('Clicou')}>
+        <LinearGradient
+          colors={['#6EA8FF', '#3D6DFF']}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Vamos Nessa!</Text>
+        </LinearGradient>
+      </TouchableOpacity>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    backgroundColor: '#fff',
+  },
+
+  logo: {
+    width: 120,
+    height: 120,
+    marginBottom: 30,
+  },
+
+  title: {
+    fontSize: 20,
+    fontWeight: '600',
+    color: '#5A8DEE',
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+
+  subtitle: {
+    fontSize: 14,
+    color: '#888',
+    marginBottom: 40,
+    textAlign: 'center',
+  },
+
+  button: {
+    paddingVertical: 15,
+    paddingHorizontal: 60,
+    borderRadius: 12,
+    width: 250,
+    alignItems: 'center',
+  },
+
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
